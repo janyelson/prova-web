45cac954468dee95cdd4b422798d148c6a33ca5b
Diego Porto <diego.cfporto@gmail.com>
2018-06-14 21:33:44 +0000

    update login layout and fix the login javascript

--- scripts/login.js
+++ scripts/login.js
@@ -1,43 +1,50 @@
 $(function(){
 
-    var api = "https://prova-api.herokuapp.com/";
-    var url = api + "login";
-    var ok = false;
+  var api = "https://prova-api.herokuapp.com/";
+  var url = api + "login";
 
-    function postJson(jsonObject)
-    {
-        xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                var myObj = JSON.parse(this.responseText);
-                if(myObj[0].username.trim) {
-                    sessionStorage.setItem("isUser", "yes");
-                    sessionStorage.setItem("email", myObj[0].email);
-                    sessionStorage.setItem("username", myObj[0].username);
-                    sessionStorage.setItem("id", myObj[0]._id);
-                    sessionStorage.setItem("type", myObj[0].type);
-                    ok = true;
-                }
-            }
-        };
+  function JSONEmpty(obj){
+    return !obj.length ||
+    !obj.filter(function(a){return Object.keys(a).length;}).length;
+  }
 
-        xmlhttp.open("POST", url, false);
-        xmlhttp.setRequestHeader("Content-type", "application/json");
-        xmlhttp.send(jsonObject);
-        return false;
-    }
+  function postJson(jsonObject) {
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      var ok = false;
+      if (this.readyState == 4 && this.status == 200) {
+        var myObj = JSON.parse(this.responseText);
+        if(!JSONEmpty(myObj)) {
+          sessionStorage.setItem("isUser", "yes");
+          sessionStorage.setItem("email", myObj[0].email);
+          sessionStorage.setItem("username", myObj[0].username);
+          sessionStorage.setItem("id", myObj[0]._id);
+          sessionStorage.setItem("type", myObj[0].type);
+          ok = true;
+        }
+        if(!ok) {
+          alert("Usuário e/ou Senha Incorretos!");
+          window.location.href = "/login";
+        }
+        else {
+          window.location.href = "/dashboard";
+        }
+      }
+    };
 
-    $("#button-login").click( function(){
-        obj = {
-            "email":$("#input-email").val(),
-            "password":$("#input-password").val(),
-        };
-        dbParam = JSON.stringify(obj);
-        postJson(dbParam);
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send(jsonObject);
+  }
 
-        if(!ok) alert("Erro, Login Incorreto!");
-        else window.location.href = "/dashboard";
+  $("#button-login").click( function(){
+    obj = {
+      "email":$("#input-email").val(),
+      "password":$("#input-password").val(),
+    };
 
-        return false;
-    });
+    dbParam = JSON.stringify(obj);
+    postJson(dbParam);
+  });
+
 });
