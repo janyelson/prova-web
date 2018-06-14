fdb1d4db71f8d7a41444acc46d60cb61d3f5d294
Diego Porto <diego.cfporto@gmail.com>
2018-06-14 10:25:06 +0000

    fix login script

--- scripts/login.js
+++ scripts/login.js
@@ -1,31 +1,13 @@
-$(function(){    
-    
+$(function(){
+
     var api = "https://prova-api.herokuapp.com/";
     var url = api + "login";
     var ok = false;
 
-    if(sessionStorage.isUser == "yes"){
-        
-        $("#login-topbar").hide();
-        $("#signup-topbar").hide();
-        
-        $("#username-topbar").text(sessionStorage.username);
-        $("#username-topbar").click( function(){
-            window.location.href= "my-page.html";
-        });
-        
-    } else
-    {
-        $("#user-name-topbar").hide();
-        $("#logout-topbar").hide();
-    }
-    
     function postJson(jsonObject)
     {
         xmlhttp = new XMLHttpRequest();
-        
         xmlhttp.onreadystatechange = function() {
-            
             if (this.readyState == 4 && this.status == 200) {
                 var myObj = JSON.parse(this.responseText);
                 if(myObj[0].username.trim) {
@@ -34,38 +16,28 @@ $(function(){
                     sessionStorage.setItem("username", myObj[0].username);
                     sessionStorage.setItem("id", myObj[0]._id);
                     sessionStorage.setItem("type", myObj[0].type);
-                
                     ok = true;
-                } 
-                
+                }
             }
         };
-        
+
         xmlhttp.open("POST", url, false);
         xmlhttp.setRequestHeader("Content-type", "application/json");
         xmlhttp.send(jsonObject);
-        
         return false;
     }
-    
-    $("#login-button").click( function(){
-        
-         
+
+    $("#button-login").click( function(){
         obj = {
-            "email":$("#email-login-input").val(), 
-            "username":"whatever",
-            "password":$("#password-login-input").val(),
-        };                               
+            "email":$("#input-email").val(),
+            "password":$("#input-password").val(),
+        };
         dbParam = JSON.stringify(obj);
-
         postJson(dbParam);
-        
-        if(!ok) alert("Erro, tente novamente!");
-        else window.location.href = "../index.html";
-        
-        return false;
-        
-    });
-    
 
+        if(!ok) alert("Erro, Login Incorreto!");
+        else window.location.href = "/dashboard";
+
+        return false;
+    });
 });
